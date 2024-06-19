d577c2be5ee2403b7b4ced1e4842ed5f5b81467e
travmonkey <travis@batk.me>
2024-06-18 20:37:48 -0600

    Finish initial keymap

--- firmware/src/keymap/keymap.cpp
+++ firmware/src/keymap/keymap.cpp
@@ -12,32 +12,32 @@ KeyMap::KeyMap(const std::string &name) {
     uint8_t keys[NUM_ROWS][NUM_COLUMNS][NUM_LAYERS] = {
         {
             // ROW 0
-            {HID_KEY_TAB, '\0', '\0', '\0'},
-            {HID_KEY_Q, HID_KEY_1, HID_KEY_P, HID_KEY_O},
-            {HID_KEY_W, HID_KEY_2, HID_KEY_F2, '\0'},
-            {HID_KEY_E, HID_KEY_3, HID_KEY_F3, '\0'},
-            {HID_KEY_R, HID_KEY_4, HID_KEY_F4, '\0'},
-            {HID_KEY_T, HID_KEY_5, HID_KEY_F5, '\0'},
+            {HID_KEY_TAB, HID_KEY_ESCAPE, HID_KEY_GRAVE, '\0'},
+            {HID_KEY_Q, HID_KEY_1, HID_KEY_P, HID_KEY_F1},
+            {HID_KEY_W, HID_KEY_2, HID_KEY_F2, HID_KEY_F2},
+            {HID_KEY_E, HID_KEY_3, HID_KEY_F3, HID_KEY_F3},
+            {HID_KEY_R, HID_KEY_4, HID_KEY_F4, HID_KEY_F4},
+            {HID_KEY_T, HID_KEY_5, HID_KEY_F5, HID_KEY_F5},
             {'\0', '\0', '\0', '\0'}, // Extra 1
         },
         {
             // ROW 1
-            {HID_KEY_SHIFT_LEFT, '\0', '\0'},
-            {HID_KEY_A, HID_KEY_2, HID_KEY_F1, '\0'},
-            {'\0', '\0',  '\0', '\0'},
-            {HID_KEY_D, HID_KEY_3, HID_KEY_F3, '\0'},
-            {HID_KEY_F, HID_KEY_4, HID_KEY_F4, '\0'},
-            {HID_KEY_G, HID_KEY_5, HID_KEY_F5, '\0'},
+            {HID_KEY_SHIFT_LEFT, '\0', '\0', HID_KEY_CAPS_LOCK},
+            {HID_KEY_A, '\0', '\0', '\0'},
+            {HID_KEY_S, '\0',  '\0', '\0'},
+            {HID_KEY_D, '\0', '\0', '\0'},
+            {HID_KEY_F, '\0', '\0', '\0'},
+            {HID_KEY_G, '\0', '\0', '\0'},
             {'\0', '\0', '\0', '\0'}, // Extra 2
         },
         {
              // ROW 2
             {HID_KEY_CONTROL_LEFT, '\0', '\0', '\0'},
-            {HID_KEY_Z, HID_KEY_1, HID_KEY_F1, '\0'},
-            {HID_KEY_X, HID_KEY_2, HID_KEY_F2, '\0'},
-            {HID_KEY_C, HID_KEY_3, HID_KEY_F3, '\0'},
-            {HID_KEY_V, HID_KEY_4, HID_KEY_F4, '\0'},
-            {HID_KEY_B, HID_KEY_5, HID_KEY_F5, '\0'},
+            {HID_KEY_Z, '\0', '\0', '\0'},
+            {HID_KEY_X, '\0', '\0', '\0'},
+            {HID_KEY_C, '\0', '\0', '\0'},
+            {HID_KEY_V, '\0', '\0', '\0'},
+            {HID_KEY_B, '\0', '\0', '\0'},
             {'\0', '\0', '\0', '\0'}, // Empty - No key here
         },
         {
@@ -45,9 +45,9 @@ KeyMap::KeyMap(const std::string &name) {
             {'\0', '\0', '\0', '\0'}, // Empty - No key here
             {'\0', '\0', '\0', '\0'}, // Empty - No key here
             {'\0', '\0', '\0', '\0'}, // Empty - No key here
-            {HID_KEY_MENU, '\0', '\0', '\0'},
+            {HID_KEY_MENU, '\0', HID_KEY_ALT_LEFT, '\0'},
             {'\0', '\0', '\0', '\0'}, // Modifier key
-            {HID_KEY_SPACE, '\0', '\0', '\0'},
+            {HID_KEY_SPACE, HID_KEY_SPACE, HID_KEY_SPACE, HID_KEY_SPACE},
         },
     };
     create_keymaps(keys);
@@ -56,35 +56,35 @@ KeyMap::KeyMap(const std::string &name) {
     uint8_t keys[NUM_ROWS][NUM_COLUMNS][NUM_LAYERS] = {
         {
             {'\0', '\0', '\0', '\0'}, // Extra 3
-            {HID_KEY_Y, '\0', '\0', '\0'},
-            {HID_KEY_U, HID_KEY_6, HID_KEY_F6, '\0'},
-            {HID_KEY_I, HID_KEY_7, HID_KEY_F7, '\0'},
-            {HID_KEY_O, HID_KEY_8, HID_KEY_F8, '\0'},
-            {HID_KEY_P, HID_KEY_9, HID_KEY_F9, '\0'},
-            {HID_KEY_BACKSPACE, HID_KEY_0, HID_KEY_F10, '\0'},
+            {HID_KEY_Y, HID_KEY_6, HID_KEY_F6, HID_KEY_F6},
+            {HID_KEY_U, HID_KEY_7, HID_KEY_F7, HID_KEY_F7},
+            {HID_KEY_I, HID_KEY_8, HID_KEY_BRACKET_LEFT, HID_KEY_F8},
+            {HID_KEY_O, HID_KEY_9, HID_KEY_BRACKET_RIGHT, HID_KEY_F9},
+            {HID_KEY_P, HID_KEY_0, HID_KEY_BACKSLASH, HID_KEY_F10},
+            {HID_KEY_BACKSPACE, HID_KEY_MINUS, HID_KEY_EQUAL, '\0'},
         },
         {
             {'\0', '\0', '\0', '\0'}, // Extra 4
-            {HID_KEY_H, '\0', '\0', '\0'},
-            {HID_KEY_J, HID_KEY_1, HID_KEY_F1, '\0'},
-            {HID_KEY_K, HID_KEY_2, HID_KEY_F2, '\0'},
-            {HID_KEY_L, HID_KEY_3, HID_KEY_F3, '\0'},
-            {HID_KEY_SEMICOLON, HID_KEY_4, HID_KEY_F4, '\0'},
-            {HID_KEY_ENTER, HID_KEY_5, HID_KEY_F5, '\0'},
+            {HID_KEY_H, HID_KEY_ARROW_LEFT, '\0', '\0'},
+            {HID_KEY_J, HID_KEY_ARROW_DOWN, HID_KEY_VOLUME_DOWN, '\0'},
+            {HID_KEY_K, HID_KEY_ARROW_UP, HID_KEY_VOLUME_UP, '\0'},
+            {HID_KEY_L, HID_KEY_ARROW_RIGHT, '\0', '\0'},
+            {HID_KEY_SEMICOLON, HID_KEY_APOSTROPHE, HID_KEY_F4, '\0'},
+            {HID_KEY_ENTER, '\0', '\0', '\0'},
         },
         {
             {'\0', '\0', '\0', '\0'}, // Empty - No key here
             {HID_KEY_N, '\0', '\0', '\0'},
-            {HID_KEY_M, HID_KEY_1, HID_KEY_F1, '\0'},
-            {HID_KEY_COMMA, HID_KEY_2, HID_KEY_F2, '\0'},
-            {HID_KEY_PERIOD, HID_KEY_3, HID_KEY_F3, '\0'},
-            {HID_KEY_SLASH, HID_KEY_4, HID_KEY_F4, '\0'},
-            {HID_KEY_SHIFT_RIGHT, HID_KEY_5, HID_KEY_F5, '\0'},
+            {HID_KEY_M, '\0', '\0', '\0'},
+            {HID_KEY_COMMA, '\0', '\0', '\0'},
+            {HID_KEY_PERIOD, '\0', '\0', '\0'},
+            {HID_KEY_SLASH, '\0', '\0', '\0'},
+            {HID_KEY_SHIFT_RIGHT, '\0', '\0', '\0'},
         },
         {
-            {HID_KEY_MENU, '\0', '\0', '\0'},
+            {HID_KEY_MENU, HID_KEY_ALT_RIGHT, '\0', '\0'},
             {'\0', '\0', '\0', '\0'}, // Modifier key
-            {HID_KEY_SPACE, '\0', '\0', '\0'},
+            {HID_KEY_SPACE, HID_KEY_SPACE, HID_KEY_SPACE, HID_KEY_SPACE},
             {'\0', '\0', '\0', '\0'}, // Empty - No key here
             {'\0', '\0', '\0', '\0'}, // Empty - No key here
             {'\0', '\0', '\0', '\0'}, // Empty - No key here
